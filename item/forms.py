ae9e55c7130aa856070e699b2a70a94e57e249ba
codexautomationkey <codexautomationkey@gmail.com>
2024-04-30 15:57:05 +0530

    item-hsn-store

--- item/forms.py
+++ item/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Item
+from market.models import Industry, Category
 
 
 INPUT_CLASSES = 'mt-6 w-auto py-3 px-3 rounded-xl border '
@@ -8,73 +9,80 @@ INPUT_CLASSES = 'mt-6 w-auto py-3 px-3 rounded-xl border '
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('item_name', 'item_description', 'image', 
-                    'preferred_unit', 'quantity', 'quality_description', 'price_per_unit',
-                    'available','item_category')
-
+        fields = [
+            'item_name', 'item_description', 'image', 'preferred_unit', 
+            'quantity', 'quality_description', 'price_per_unit', 
+            'available', 'expiration_date', 'item_category'
+        ]
         labels = {
-            'quantity': 'M.O.Q.'
+            'quantity': 'Minimum Order Quantity (MOQ)',
+            'item_category': 'Category'
         }
-
         widgets = {
-            'item_category':forms.Select(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'item_name': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'item_description': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'image': forms.FileInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-
-
+            'item_name': forms.TextInput(attrs={'class': 'input-class'}),
+            'item_description': forms.Textarea(attrs={'class': 'input-class'}),
+            'image': forms.FileInput(attrs={'class': 'input-class'}),
+            'preferred_unit': forms.Select(attrs={'class': 'input-class'}),
+            'quantity': forms.NumberInput(attrs={'class': 'input-class'}),
+            'quality_description': forms.Textarea(attrs={'class': 'input-class'}),
+            'price_per_unit': forms.NumberInput(attrs={'class': 'input-class'}),
+            'available': forms.CheckboxInput(attrs={'class': 'input-class'}),
+            'expiration_date': forms.DateInput(attrs={'class': 'input-class', 'type': 'date'}),
+            'item_category': forms.Select(attrs={'class': 'input-class'})
         }
+
+    def __init__(self, *args, **kwargs):
+        super(NewItemForm, self).__init__(*args, **kwargs)
+        self.fields['item_category'].queryset = Category.objects.all()
+        self.fields['item_category'].label_from_instance = lambda obj: f"{obj.hsn_6_digit} - {obj.category_name[:20]}"
 
     def save(self, commit=True):
         instance = super(NewItemForm, self).save(commit=False)
-        # Set item's industry based on the selected category's industry
         if instance.item_category:
             instance.item_industry = instance.item_category.industry
         if commit:
             instance.save()
-            # Save many-to-many data for the form.
             self._save_m2m()
         return instance
+
 
 class EditItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('item_category', 'item_name', 'item_description', 'image', 'preferred_unit', 'quantity', 'quality_description', 'price_per_unit',
-                    'available',)
-
+        fields = [
+            'item_name', 'item_description', 'image', 
+            'preferred_unit', 'quantity', 'quality_description', 'price_per_unit',
+            'available', 'item_category'
+        ]
         labels = {
-            'quantity' : 'M.O.Q'
+            'quantity': 'Minimum Order Quantity (MOQ)'
         }
-        
         widgets = {
+            'item_category': forms.Select(attrs={'class': 'input-class'}),
+            'item_name': forms.TextInput(attrs={'class': 'input-class'}),
+            'item_description': forms.Textarea(attrs={'class': 'input-class'}),
+            'image': forms.FileInput(attrs={'class': 'input-class'}),
+            'preferred_unit': forms.Select(attrs={'class': 'input-class'}),
+            'quantity': forms.NumberInput(attrs={'class': 'input-class'}),
+            'quality_description': forms.Textarea(attrs={'class': 'input-class'}),
+            'price_per_unit': forms.NumberInput(attrs={'class': 'input-class'}),
+            'available': forms.CheckboxInput(attrs={'class': 'input-class'}),
             
-            'item_category':forms.Select(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'item_name': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'item_description': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'image': forms.FileInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'quantity': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            })
-
-
         }
 
+    def __init__(self, *args, **kwargs):
+        super(EditItemForm, self).__init__(*args, **kwargs)
+        self.fields['item_category'].queryset = Category.objects.all()
+        self.fields['item_category'].label_from_instance = lambda obj: f"{obj.hsn_6_digit} - {obj.category_name[:20]}"
+
+    def save(self, commit=True):
+        instance = super(EditItemForm, self).save(commit=False)
+        if instance.item_category:
+            instance.item_industry = instance.item_category.industry
+        if commit:
+            instance.save()
+            self._save_m2m()
+        return instance
 
 
 
